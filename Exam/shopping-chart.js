917c4ea302177121d9b51d497c917f36dc2ba2e4
Murito-01 <ristoo.abrar@gmail.com>
2026-07-05 14:55:15 +0700

    Add shopping cart functionality with product listing

---
+++ Exam/shopping-chart.js
@@ -0,0 +1,21 @@
+const products = ['Apel', 'Mangga', 'Jeruk', 'Anggur', 'Nanas'];
+const prices = [5000, 4000, 3000, 15000, 20000];
+
+const productList = products.map((item, index) => {
+  return {
+    name: item,
+    price: prices[index]
+  };
+});
+
+let item = '';
+for (let i = 0; i < productList.length; i++) {
+  item += `
+    <div>
+      ${productList[i].name} - Rp ${productList[i].price}
+      <button>+ Add</button>
+    </div>
+  `;
+}
+
+document.querySelector('.js-product-list').innerHTML = item;
