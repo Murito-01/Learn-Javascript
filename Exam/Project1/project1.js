563ff059dd1be496f6d289eb129a370898b95cac
Murito-01 <ristoo.abrar@gmail.com>
2026-07-07 10:37:12 +0700

    Add initial implementation for calculating variable

---
+++ Exam/Project1/project1.js
@@ -0,0 +1,13 @@
+// List variable nilai
+const nilai1 = 85;
+const nilai2 = 80;
+const nilai3 = 95;
+const nilai4 = 60;
+const nilai5 = 70;
+
+// Mean of nilai
+console.log(`Rata-rata nilai kelas: ${(nilai1 + nilai2 + nilai3 + nilai4 + nilai5) / 5}`);
+// Max of nilai
+console.log(`Nilai tertinggi: ${Math.max(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
+// Min of nilai
+console.log(`Nilai tertinggi: ${Math.min(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
