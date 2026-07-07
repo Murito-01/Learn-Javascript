6a898244d74ffe1d301b7d001e1c5d7af3cb1725
Murito-01 <ristoo.abrar@gmail.com>
2026-07-07 10:48:00 +0700

    Refactor to implement function

--- Exam/Project1/project1.js
+++ Exam/Project1/project1.js
@@ -7,22 +7,27 @@ const nilai5 = 70;
 
 const mean = (nilai1 + nilai2 + nilai3 + nilai4 + nilai5) / 5;
 
-// Mean of nilai
-console.log(`Rata-rata nilai kelas: ${mean}`);
-// Max of nilai
-console.log(`Nilai tertinggi: ${Math.max(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
-// Min of nilai
-console.log(`Nilai tertinggi: ${Math.min(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
+// Function
+function hitungNilai () {
+  // Mean of nilai
+  console.log(`Rata-rata nilai kelas: ${mean}`);
+  // Max of nilai
+  console.log(`Nilai tertinggi: ${Math.max(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
+  // Min of nilai
+  console.log(`Nilai tertinggi: ${Math.min(nilai1, nilai2, nilai3, nilai4, nilai5)}`);
 
-//Variable and Booleans
-if (mean >= 90) {
-  console.log('Grade A');
-} else if (mean >= 80) {
-  console.log('Grade B');
-} else if (mean >= 70) {
-  console.log('Grade C');
-} else if (mean >= 60) {
-  console.log('Grade D');
-} else {
-  console.log('Grade E');
-};
+  //Variable and Booleans
+  if (mean >= 90) {
+    console.log('Grade A');
+  } else if (mean >= 80) {
+    console.log('Grade B');
+  } else if (mean >= 70) {
+    console.log('Grade C');
+  } else if (mean >= 60) {
+    console.log('Grade D');
+  } else {
+    console.log('Grade E');
+  };
+}
+
+hitungNilai();
